d31ca58f7d699d1ca2bd0d2c18c45b2800c65795
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-13 23:45:45 +0700

    add middleware

--- src/app/router/router.go
+++ src/app/router/router.go
@@ -21,13 +21,17 @@ type IGuard interface {
 func NewGinRouter(authGuard IGuard, conf config.App) *GinRouter {
 	r := gin.Default()
 
-	user := r.Group("/user")
-	// user.Use(middleware.AuthMiddleware())
-	auth := r.Group("/auth")
-
-	like := r.Group("/like")
-	emoji := r.Group("/emoji")
-	rating := r.Group("/rating")
+	user := GroupWithAuthMiddleware(r, "/user", authGuard.Use)
+	auth := GroupWithAuthMiddleware(r, "/auth", authGuard.Use)
+	like := GroupWithAuthMiddleware(r, "/like", authGuard.Use)
+	emoji := GroupWithAuthMiddleware(r, "/emoji", authGuard.Use)
+	rating := GroupWithAuthMiddleware(r, "/rating", authGuard.Use)
 
 	return &GinRouter{r, user, auth, like, emoji, rating}
+}
+
+func GroupWithAuthMiddleware(r *gin.Engine, path string, middleware func(ctx *GinCtx)) *gin.RouterGroup {
+	return r.Group(path, func(c *gin.Context) {
+		middleware(NewGinCtx(c))
+	})
 }
