1c16902ba9e42d91b18033a1a2b0a7f170077448
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-14 15:08:00 +0700

    fix: unauth routes

--- src/constant/auth/auth.constant.go
+++ src/constant/auth/auth.constant.go
@@ -1,11 +1,11 @@
 package auth
 
 var ExcludePath = map[string]struct{}{
-	"POST /auth/verify":       {},
-	"POST /auth/refreshToken": {},
-	"GET /problem":            {},
-	"GET /course":             {},
-	"GET /like/:userId":       {},
-	"GET /emoji":              {},
-	"GET /rating":             {},
+	"POST /auth/verify/":       {},
+	"POST /auth/refreshToken/": {},
+	"GET /problem/":            {},
+	"GET /course/":             {},
+	// "GET /like/:userId":       {},
+	"GET /emoji/":  {},
+	"GET /rating/": {},
 }
