84e9a03b6d819848fdaba8b305966458b77984e9
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-16 13:08:17 +0700

    fix: add hedirecturl to non auth

--- src/constant/auth/auth.constant.go
+++ src/constant/auth/auth.constant.go
@@ -4,6 +4,7 @@ var ExcludePath = map[string]struct{}{
 	"POST /auth/verify":       {},
 	"POST /auth/refreshToken": {},
 	"POST /auth/googleUrl":    {},
+	"POST /auth/verifyGoogle": {},
 	"GET /problem/":           {},
 	"GET /course/":            {},
 	// "GET /like/:userId":       {},
